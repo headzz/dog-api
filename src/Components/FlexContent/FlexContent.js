375ef574db2c6c628792c1f781619470f3ff49dc
Pedro <headbangzz2@gmail.com>
2019-11-06 02:45:04 -0300

    criação de novas propriedades dinâmicas para flexbox e ajuste de suas respectivas proptypes

--- src/Components/FlexContent/FlexContent.js
+++ src/Components/FlexContent/FlexContent.js
@@ -4,12 +4,25 @@ import styled from 'styled-components';
 
 const FlexStyled = styled.div`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: ${props => props.justify};
+  align-items: ${props => props.align};
 `;
-const FlexContent = ({ children }) => <FlexStyled>{children}</FlexStyled>;
+
+const FlexContent = ({ children, justify, align }) => (
+  <FlexStyled justify={justify} align={align}>
+    {children}
+  </FlexStyled>
+);
 
 FlexContent.propTypes = {
-  children: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  justify: PropTypes.string,
+  align: PropTypes.string,
+};
+
+FlexContent.defaultProps = {
+  justify: '',
+  align: '',
 };
 
 export default FlexContent;
