901df7cfff32ee6a98ebb9d83822bf8a18edc63b
Pedro <headbangzz2@gmail.com>
2019-11-06 02:43:17 -0300

    Ajuste de fonte no header e inserção das variáveis de cor

--- src/Components/Template/styled/Template.styled.js
+++ src/Components/Template/styled/Template.styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import { YELLOW_BACKGROUND_COLOR } from '../../Color/Color';
 
 export const BodyStyled = styled.div`
+  background-color: ${YELLOW_BACKGROUND_COLOR}
   padding: 4em;
   width: 100%;
 `;
@@ -10,6 +12,9 @@ export const ContentStyled = styled.div`
   width: 100%;
 `;
 
-export const HeaderStyled = styled.div`
-  height: 2em;
+export const HeaderContent = styled.header`
+  @import url('https://fonts.googleapis.com/css?family=Oswald&display=swap');
+  font-family: Oswald, sans-serif;
+  font-size: 2rem;
+  padding: 0rem 1rem;
 `;
