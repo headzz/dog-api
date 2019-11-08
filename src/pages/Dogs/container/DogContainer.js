ea1a551dfbfc4abccc31147750e66e1e8ffdac1a
Pedro <headbangzz2@gmail.com>
2019-11-08 02:36:59 -0300

    container agora isola o que exige em relação a busca de dados

--- src/pages/Dogs/container/DogContainer.js
+++ src/pages/Dogs/container/DogContainer.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Template from '../../../Components/Template/index';
 import Header from '../components/Header/Header';
 import Body from '../components/Body/Body';
+import { API_GET_DOG } from '../../../config/constants';
+import useFetch from '../../../utils/hooks/useFetch';
 
-const DogContainer = () => (
-  <>
-    <Template.Header>
-      <Header />
-    </Template.Header>
-    <Template.Body>
-      <Body />
-    </Template.Body>
-  </>
-);
+const DogContainer = () => {
+  const fetch = useFetch(API_GET_DOG);
+  return (
+    <>
+      <Template.Header>
+        <Header />
+      </Template.Header>
+      <Template.Body>
+        <Body fetch={fetch} />
+      </Template.Body>
+    </>
+  );
+};
 
 export default DogContainer;
