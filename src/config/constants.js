b401e78f7ba9d3715c2001954576ab5c60466783
Pedro <headbangzz2@gmail.com>
2019-11-08 02:40:20 -0300

    criação de alguns arquivos estáticos para facilitar configuração

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,18 @@
 export const NOT_FOUND = 404;
+export const API_BREED_IMAGE_BASE_LINK = 'https://dog.ceo/api/breed/';
+export const API_BREED_IMAGE_END_LINK = '/images/random';
+export const API_GET_DOG = 'https://dog.ceo/api/breeds/list/all';
+export const FONTS = [
+  'Montserrat',
+  'ZCOOL XiaoWei',
+  'Staatliches',
+  'Supermercado One',
+  'Big Shoulders',
+];
+export const SELECT_COLORS = [
+  { description: 'Verde', value: '#00A32C' },
+  { description: 'Azul', value: '#0008A3' },
+  { description: 'Roxo', value: '#960099' },
+  { description: 'Laranja', value: '#C25811' },
+  { description: 'Preto', value: '#171717' },
+];
