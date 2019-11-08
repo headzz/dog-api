4a8666a1297bf1f7e0ecf262098be369d137099b
Pedro <headbangzz2@gmail.com>
2019-11-08 02:35:25 -0300

    simplificando o tratamento do dado vindo do fetch

--- src/utils/hooks/useFetch.js
+++ src/utils/hooks/useFetch.js
@@ -1,22 +1,39 @@
 import { useState, useEffect } from 'react';
 import { NOT_FOUND } from '../../config/constants';
 
-const useFetch = () => {
+const useFetch = API_LINK => {
   const [loading, setLoading] = useState(true);
   const [dogData, setDogData] = useState([]);
   const [error, setError] = useState(false);
   const [message, setMessage] = useState('');
 
+  const formatResponse = response => {
+    const dogsName = [];
+    const dogsRaces = Object.entries(response);
+    for (let i = 0; i < dogsRaces.length; i += 1) {
+      const merge = dogsRaces[i].flat(1);
+      const name = merge.shift();
+      if (merge.length !== 0) {
+        for (let k = 0; k < merge.length; k += 1) {
+          dogsName.push(`${name}-${merge[k]}`);
+          setDogData(dogsName);
+        }
+      } else {
+        dogsName.push(`${name}`);
+        setDogData(dogsName);
+      }
+    }
+    setLoading(false);
+  };
+
   const fetchData = async () => {
     try {
-      const response = await fetch('https://dog.ceo/api/breeds/list/all');
+      const response = await fetch(API_LINK);
       const json = await response.json();
       if (json.code === NOT_FOUND) {
         setMessage('Conteúdo não encontrado');
       }
-
-      setDogData(json.message);
-      setLoading(false);
+      formatResponse(json.message);
     } catch (e) {
       setError(true);
     }
